07bd2dcb8a9f39883604ef55181e48459fde0857
nakulswami <nakulswami92000@gmail.com>
2026-08-26 10:47:14 +0530

    Show enquiry error

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,7 @@ app.delete("/api/enquiries/:id", async (req, res) => {
         console.error(error);
 
         res.status(500).json({
-            message: "Failed to delete enquiry",
+            message: error.message,
         });
     }
 });
