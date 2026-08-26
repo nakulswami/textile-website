cd33e823e84b7a2cc4b47802442934360df1c929
nakulswami <nakulswami92000@gmail.com>
2026-08-26 10:32:01 +0530

    Add admin routes

--- textile-website/src/App.jsx
+++ textile-website/src/App.jsx
@@ -1,6 +1,18 @@
 import Home from './pages/Home'
+import Admin from './pages/Admin'
+import AdminLogin from './pages/AdminLogin'
 
 function App() {
+  const path = window.location.pathname
+
+  if (path === '/admin') {
+    return <Admin />
+  }
+
+  if (path === '/admin/login') {
+    return <AdminLogin />
+  }
+
   return <Home />
 }
 
